17d37b9444aaa18e29e93dd0e9547061575bc976
vlniksa <124531703+vlniksa@users.noreply.github.com>
2023-10-11 17:44:31 +0300

    Add files via upload

--- calc.java
+++ calc.java
@@ -1,451 +1,342 @@
-
+package Calc;
 import java.util.Scanner;
 
-public class calc {
-
-		public static void main (String[] args) {
-		//StringBuilder virazhenie = new StringBuilder("VIII / VIII");
-		String inputstring;
-		StringBuilder virazhenie = new StringBuilder("");
-		String vir="" ;
-		String [] words;
-		String strspace = " "; 
-		String ourstring ;
-		int edenitsi=0;
-		StringBuilder resultrim = new StringBuilder(""); 
-		int operand1 =0;
-		int operand2 =0;
-		String strop1;
-		String strop2;
-		String strop;
-		int indSpace = 0;
-		int lenvir = 0;
-		int rezult=0;
-		boolean correct= false, arab= false, rim= false, oparab1= false, oparab2= false, oprim1= false, oprim2= false;
-
+public class calc{
+	public static void main (String[] args) {
+//	while (true) {
+		String inputstring="",virajenie="";
+		String[] word_len;
+		String rezult="";
+		int tryer=0;
 		System.out.println("Для получения результата введите арифметическое выражение в виде x * y,");
 		System.out.println("где x и y могут быть арабские, либо римские числа от 1 до 10, ");
-		System.out.println("в качестве * могут выступать операторы +,-,*,/");
-		
+		System.out.println("в качестве * могут выступать операторы +,-,*,/ \n");
 		Scanner input = new Scanner(System.in); //ввод выражения
 		inputstring=input.nextLine();//в строку
 		input.close();//закрываем портал
-		
-		virazhenie.append(inputstring); //копируем в  StringBuilder для возможности редактирования
-		vir=virazhenie.toString();
-		lenvir=virazhenie.length(); //размер (длина) строки
-		indSpace=virazhenie.indexOf(strspace);
-		
-		/*if (lenvir<5 || indSpace<1 ){
-			System.out.println("Неверный формат ввода данных или строка не является математической операцией" );
-			return;
-		}
-		*/
-		words=vir.split(" ");
-		//System.out.println(words.length);
-		if ( words.length!=3) {
-			return;
-		}
-		for (byte i=0; i<words.length; i++) {
-			System.out.println(words[i]);
-		}
-		
-		
-		indSpace=virazhenie.indexOf(strspace); //ищем позицию пробела
-		ourstring=virazhenie.substring(0, indSpace); //выбираем из строки первый операнд
-		strop1=ourstring.toString(); //1 операнд присваиваем строке 
-		
-		if (indSpace+1<=lenvir) { //соизмеряем со строкой позицию пробела и если она не больше то удаляем символы до первого пробела включительно
-			virazhenie=virazhenie.delete(0, indSpace+1);
-		}
-		indSpace=virazhenie.indexOf(strspace);//ищем позицию пробела
-		ourstring=virazhenie.substring(0, indSpace);//выбираем из строки операцию
-		strop=ourstring;
-		lenvir=virazhenie.length();
-		if (indSpace+1<=lenvir) {//соизмеряем со строкой позицию пробела и если она не больше то удаляем символы до второго пробела включительно
-			virazhenie=virazhenie.delete(0, indSpace+1);
-		}
-		ourstring=virazhenie.toString(); //выбираем из строки операнд 2
-		strop2=ourstring;
-		
-		if (   (strop1.equals("1") //проверка на соотвествие 1-го операнда
-				|| strop1.equals("2") 
-				|| strop1.equals("3") 
-				|| strop1.equals("4") 
-				|| strop1.equals("5") 
-				|| strop1.equals("6") 
-				|| strop1.equals("7") 
-				|| strop1.equals("8") 
-				|| strop1.equals("9") 
-				|| strop1.equals("10"))
-				){
-			oparab1=true;
-			correct=true;
-		}
-				
-		if (	(strop2.equals("1") //проверка на соотвествие 2-го операнда
-				|| strop2.equals("2") 
-				|| strop2.equals("3") 
-				|| strop2.equals("4") 
-				|| strop2.equals("5") 
-				|| strop2.equals("6") 
-				|| strop2.equals("7") 
-				|| strop2.equals("8") 
-				|| strop2.equals("9") 
-				|| strop2.equals("10"))
-				) {
-			oparab2=true;
-			correct=true;
-		}
+		virajenie=inputstring.trim();
+		word_len=inputstring.split(" ");
+//		if(virajenie=="999") {
+//			break;
+//		} 		
+		if ( word_len.length!=3) {
+			try { 
+				tryer= 1/0;
 
-		if ((	strop1.equals("I") //проверка на соотвествие 1-го операнда
-				|| strop1.equals("II") 
-				|| strop1.equals("III") 
-				|| strop1.equals("IV" )
-				|| strop1.equals("V" )
-				|| strop1.equals("VI" )
-				|| strop1.equals("VII" )
-				|| strop1.equals("VIII" )
-				|| strop1.equals("IX" )
-				|| strop1.equals("X"))
-				) {
-			oprim1=true;
-			correct=true;
+				} catch (Exception e) {
+					System.out.println("Формат математической операции не удовлетворяет заданию" );
+				}
+			return;
+		} else
+			rezult=calc(inputstring);
+		} //end if
+//	}
+	
+	
+	public static String calc(String input) {
+		byte operand1=0;
+		byte operand2=0;
+		int edenitsi=0;
+		StringBuilder rezult = new StringBuilder(""); 
+		String[] word;
+		boolean arab = false, rim =false ;
+		int rezult_calc = 0;
+		int tryer=0;
+		word=input.split(" ");
+		operand1=checkop(word[0]);
+		operand2=checkop(word[2]);
+		if (operand1<=0 || operand1>10 || operand2<=0 || operand2>10) {
+			try {
+				tryer = 1/0	;
+				} catch (Exception e) {
+					System.out.println("Формат математической операции не удовлетворяет заданию: числа должны быть от 1 до 10");
+				}
+			return "";
 		}
-				
-		if ((	strop2.equals("I") //проверка на соотвествие 2-го операнда
-				|| strop2.equals("II") 
-				|| strop2.equals("III") 
-				|| strop2.equals("IV" )
-				|| strop2.equals("V" )
-				|| strop2.equals("VI" )
-				|| strop2.equals("VII" )
-				|| strop2.equals("VIII" )
-				|| strop2.equals("IX" )
-				|| strop2.equals("X"))
-				) {
-			oprim2=true;
-			correct=true;
-		}
-
-		if ((	strop.equals("+") 
-				|| strop.equals("-") 
-				|| strop.equals("*")
-				|| strop.equals("/")
+		if (  	(word[0].equals("1") //проверка арабский 
+				|| word[0].equals("2") 
+				|| word[0].equals("3") 
+				|| word[0].equals("4") 
+				|| word[0].equals("5") 
+				|| word[0].equals("6") 
+				|| word[0].equals("7") 
+				|| word[0].equals("8") 
+				|| word[0].equals("9") 
+				|| word[0].equals("10"))
+				 &&
+				(word[2].equals("1") //  
+				|| word[2].equals("2") 
+				|| word[2].equals("3") 
+				|| word[2].equals("4") 
+				|| word[2].equals("5") 
+				|| word[2].equals("6") 
+				|| word[2].equals("7") 
+				|| word[2].equals("8") 
+				|| word[2].equals("9") 
+				|| word[2].equals("10"))
 				)
-			){
-			correct=true;
-		}
-		else {
-			System.out.println("Неверное, либо отсутствует арифметическое действие");
-			correct=false;
-		}
-		
-		if (!(oparab1 && oparab2) && !(oprim1 && oprim2)) {
-			System.out.println("Используются одновременно разные системы счисления");
-			correct=false;
-		} 
-		if (oparab1 && oparab2) {//цифры арабские
+		{ //начало выполнения  if
 			arab=true;
-		}
-		if (oprim1 && oprim2) { //цифры римские
-			rim=true;
-		}
-		if ( !correct) {
-			System.out.println("Неверный формат ввода данных или строка не является математической операцией" );
-			return;
-		}
-		if (arab) {// преобразование из строки в целое число 
-			switch (strop1) {
-			case ("1"):{	
-				operand1=1;
-				break;
-				}
-			case ("2"):{	
-				operand1=2;
-				break;
-				}
-			case ("3"):{	
-				operand1=3;
-				break;
-				}
-			case ("4"):{	
-				operand1=4;
-				break;
-				}
-			case ("5"):{	
-				operand1=5;
-				break;
-				}
-			case ("6"):{	
-				operand1=6;
-				break;
-				}
-			case ("7"):{	
-				operand1=7;
-				break;
-				}
-			case ("8"):{	
-				operand1=8;
-				break;
-				}
-			case ("9"):{	
-				operand1=9;
-				break;
-				}
-			case ("10"):{	
-				operand1=10;
-				break;
-				}
+			rim=false;
+		}	else { if ( (word[0].equals("I") //проверка римский 
+						|| word[0].equals("II") 
+						|| word[0].equals("III") 
+						|| word[0].equals("IV") 
+						|| word[0].equals("V") 
+						|| word[0].equals("VI") 
+						|| word[0].equals("VII") 
+						|| word[0].equals("VII") 
+						|| word[0].equals("IX") 
+						|| word[0].equals("X"))
+				 		&&
+				 		(word[2].equals("I")  
+				 		|| word[2].equals("II") 
+				 		|| word[2].equals("III") 
+				 		|| word[2].equals("IV") 
+				 		|| word[2].equals("V") 
+				 		|| word[2].equals("VI") 
+				 		|| word[2].equals("VII") 
+				 		|| word[2].equals("VII") 
+				 		|| word[2].equals("IX") 
+				 		|| word[2].equals("X")) ) 
+						{ //начало выполнения  if
+						arab=false;
+						rim=true;
+				} //окончание выполнения  if
+					else {
+						try { 
+							tryer= 1/0;
+				
+						} catch (Exception e) {
+							// TODO: handle exception
+							System.out.println("Используются одновременно разные системы счисления" );
+						} 
+						return "";
 			}
-			switch (strop2) {
-			case ("1"):{	
-				operand2=1;
-				break;
-				}
-			case ("2"):{	
-				operand2=2;
-				break;
-				}
-			case ("3"):{	
-				operand2=3;
-				break;
-				}
-			case ("4"):{	
-				operand2=4;
-				break;
-				}
-			case ("5"):{	
-				operand2=5;
-				break;
-				}
-			case ("6"):{	
-				operand2=6;
-				break;
-				}
-			case ("7"):{	
-				operand2=7;
-				break;
-				}
-			case ("8"):{	
-				operand2=8;
-				break;
-				}
-			case ("9"):{	
-				operand2=9;
-				break;
-				}
-			case ("10"):{	
-				operand2=10;
-				break;
-				}
-			}
-	
-		}
-		if (rim) {// преобразование из строки в целое число
-			switch (strop1) {
-			case ("I"):{	
-				operand1=1;
-				break;
-				}
-			case ("II"):{	
-				operand1=2;
-				break;
-				}
-			case ("III"):{	
-				operand1=3;
-				break;
-				}
-			case ("IV"):{	
-				operand1=4;
-				break;
-				}
-			case ("V"):{	
-				operand1=5;
-				break;
-				}
-			case ("VI"):{	
-				operand1=6;
-				break;
-				}
-			case ("VII"):{	
-				operand1=7;
-				break;
-				}
-			case ("VIII"):{	
-				operand1=8;
-				break;
-				}
-			case ("IX"):{	
-				operand1=9;
-				break;
-				}
-			case ("X"):{	
-				operand1=10;
-				break;
-				}
-			}
-			switch (strop2) {
-			case ("I"):{	
-				operand2=1;
-				break;
-				}
-			case ("II"):{	
-				operand2=2;
-				break;
-				}
-			case ("III"):{	
-				operand2=3;
-				break;
-				}
-			case ("IV"):{	
-				operand2=4;
-				break;
-				}
-			case ("V"):{	
-				operand2=5;
-				break;
-				}
-			case ("VI"):{	
-				operand2=6;
-				break;
-				}
-			case ("VII"):{	
-				operand2=7;
-				break;
-				}
-			case ("VIII"):{	
-				operand2=8;
-				break;
-				}
-			case ("IX"):{	
-				operand2=9;
-				break;
-				}
-			case ("X"):{	
-				operand2=10;
-				break;
-				}
-			}//switch
-	
-		}//if
-		//System.out.println(operand1);
-		//System.out.println(operand2);
-		switch (strop) { //вычисление 
+		}//окончание выполнения  if
+		switch (word[1]) { //вычисление 
 		case ("+"):{	
-			rezult=operand1+operand2;
+			rezult_calc=operand1+operand2;
 			break;
 			}
 		case ("-"):{
-			rezult=operand1-operand2;
-			if (rim && rezult<0) { // проверка на орицательное число в результате
-				System.out.println("в римской системе нет отрицательных чисел");
-				return;
+			rezult_calc=operand1-operand2;
+			if (rim && rezult_calc<0) { // проверка на орицательное число в результате
+				try {
+					tryer = 1/0	;
+					} catch (Exception e) {
+					// TODO: handle exception
+						System.out.println("В римской системе нет отрицательных чисел");
+					}
+				return "";
 				}
 			break;
 			}
 		case ("/"):{	
-			rezult=operand1 / operand2;
+			rezult_calc=operand1 / operand2;
 			break;
 			}
 		case ("*"):{	
-			rezult=operand1 * operand2;
+			rezult_calc=operand1 * operand2;
 			break;
 			}
+		default:
+			try {
+				tryer = 1/0	;
+				} catch (Exception e) {
+				// TODO: handle exception
+					System.out.println("Формат математической операции не удовлетворяет заданию");
+				}
+			return "";
 		}//switch
-		//System.out.println(rezult);
-		
 		if (rim) {// если были введены римские, то преобразуем результат в римском формате
-				edenitsi = rezult%10; //находим единицы и присваиваем соответствующие римские цифры 
-				switch (edenitsi) {
-				case(1): 
-					{
-					resultrim.append("I");
-					break;
-					}
-				case(2): 
-					{
-					resultrim.append("II");
-					break;
-					}
-				case(3): 
-					{
-					resultrim.append("III");
-					break;
-					}
-				case(4): 
-					{
-					resultrim.append("IV");
-					break;
-					}
-				case(5): 
-					{
-					resultrim.append("V");
-					break;
-					}
-				case(6): 
-					{
-					resultrim.append("VI");
-					break;
-					}
-				case(7): 
-					{
-					resultrim.append("VII");
-					break;
-					}
-				case(8): 
-					{
-					resultrim.append("VIII");
-					break;
-					}
-				case(9): 
-					{
-					resultrim.append("IX");
-					break;
-					}
-				default:
-					resultrim.append("Пусто");
+			edenitsi = rezult_calc%10; //находим единицы и присваиваем соответствующие римские цифры 
+			switch (edenitsi) {
+			case(1): 
+				{
+				rezult.append("I");
+				break;
 				}
-				
-				if (rezult>=10 && rezult<20) { //добавляем десятки
-					resultrim.insert(0,"X");
+			case(2): 
+				{
+				rezult.append("II");
+				break;
 				}
-				if (rezult>=20 && rezult<30) {
-					resultrim.insert(0,"XX");
+			case(3): 
+				{
+				rezult.append("III");
+				break;
 				}
-				if (rezult>=30 && rezult<40) {
-					resultrim.insert(0,"XXX");
+			case(4): 
+				{
+				rezult.append("IV");
+				break;
 				}
-				if (rezult>=40 && rezult<50) {
-					resultrim.insert(0,"XL");
+			case(5): 
+				{
+				rezult.append("V");
+				break;
 				}
-				if (rezult>=50 && rezult<60) {
-					resultrim.insert(0,"L");
+			case(6): 
+				{
+				rezult.append("VI");
+				break;
 				}
-				if (rezult>=60 && rezult<70) {
-					resultrim.insert(0,"LХ");
+			case(7): 
+				{
+				rezult.append("VII");
+				break;
 				}
-				if (rezult>=70 && rezult<80) {
-					resultrim.insert(0,"LХX");
+			case(8): 
+				{
+				rezult.append("VIII");
+				break;
 				}
-				if (rezult>=80 && rezult<90) {
-					resultrim.insert(0,"LХXX");
+			case(9): 
+				{
+				rezult.append("IX");
+				break;
 				}
-				if (rezult>=90 && rezult<100) {
-					resultrim.insert(0,"ХC");
+			case(10): 
+				{
+				rezult.append("X");
+				break;
 				}
-				if (rezult==100) {
-					resultrim.insert(0,"C");
-				}
+			}
+			
+			if (rezult_calc>=10 && rezult_calc<20) { //добавляем десятки
+				rezult.insert(0,"X");
+			}
+			if (rezult_calc>=20 && rezult_calc<30) {
+				rezult.insert(0,"XX");
+			}
+			if (rezult_calc>=30 && rezult_calc<40) {
+				rezult.insert(0,"XXX");
+			}
+			if (rezult_calc>=40 && rezult_calc<50) {
+				rezult.insert(0,"XL");
+			}
+			if (rezult_calc>=50 && rezult_calc<60) {
+				rezult.insert(0,"L");
+			}
+			if (rezult_calc>=60 && rezult_calc<70) {
+				rezult.insert(0,"LХ");
+			}
+			if (rezult_calc>=70 && rezult_calc<80) {
+				rezult.insert(0,"LХX");
+			}
+			if (rezult_calc>=80 && rezult_calc<90) {
+				rezult.insert(0,"LХXX");
+			}
+			if (rezult_calc>=90 && rezult_calc<100) {
+				rezult.insert(0,"ХC");
+			}
+			if (rezult_calc==100) {
+				rezult.insert(0,"C");
+			}
 
-				System.out.println("Результат равен : "+ resultrim); 
-		} else {
+			System.out.println("Результат равен : "+ rezult); 
+			
+	} else { if (arab) {
+			rezult.append(rezult_calc);
 			System.out.println("Результат равен : "+ rezult);
+			}
+			;
 		} //if
 		
-		
-		} //public void
-} //public class
-
+	return rezult.toString();
+	}
+	
+	// Метод (выставление соотвествий содердимого стрингового выражения и byte)
+	public static byte checkop(String input) {
+		byte var_checkop=0;
+		int tryer;
+		switch (input) {
+		case ("1"):{	
+			var_checkop=1;
+			break;
+			}
+		case ("2"):{	
+			var_checkop=2;
+			break;
+			}
+		case ("3"):{	
+			var_checkop=3;
+			break;
+			}
+		case ("4"):{	
+			var_checkop=4;
+			break;
+			}
+		case ("5"):{	
+			var_checkop=5;
+			break;
+			}
+		case ("6"):{	
+			var_checkop=6;
+			break;
+			}
+		case ("7"):{	
+			var_checkop=7;
+			break;
+			}
+		case ("8"):{	
+			var_checkop=8;
+			break;
+			}
+		case ("9"):{	
+			var_checkop=9;
+			break;
+			}
+		case ("10"):{	
+			var_checkop=10;
+			break;
+			}
+		case ("I"):{	
+			var_checkop=1;
+			break;
+			}
+		case ("II"):{	
+			var_checkop=2;
+			break;
+			}
+		case ("III"):{	
+			var_checkop=3;
+			break;
+			}
+		case ("IV"):{	
+			var_checkop=4;
+			break;
+			}
+		case ("V"):{	
+			var_checkop=5;
+			break;
+			}
+		case ("VI"):{	
+			var_checkop=6;
+			break;
+			}
+		case ("VII"):{	
+			var_checkop=7;
+			break;
+			}
+		case ("VIII"):{	
+			var_checkop=8;
+			break;
+			}
+		case ("IX"):{	
+			var_checkop=9;
+			break;
+			}
+		case ("X"):{	
+			var_checkop=10;
+			break;
+			}
+		default:
+			var_checkop=0;
+		}
+		return var_checkop;
+	}
+}
 
 
